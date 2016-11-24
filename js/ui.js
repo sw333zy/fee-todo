6df22e5b3ff9fc63d4cf2b9124f8d3897ff338a8
J Ridgeway <sw333zy@gmail.com>
2016-11-24 15:45:13 -0500

    delete works

--- js/ui.js
+++ js/ui.js
@@ -1,46 +1,44 @@
 (function() {
-  'use strict';
-  window.todo = window.todo || {};
+    'use strict';
+    window.todo = window.todo || {};
 
 
 
     function addToDo(text) {
-      var li = document.createElement('li');
-      var article = document.createElement('article');
-      var button = document.createElement('button');
-      button.classList.add('check');
-      article.appendChild(button);
-      var p = document.createElement('p');
-      p.innerText = text;
-      article.appendChild(p);
-      var deleteb = document.createElement('button');
-      deleteb.classList.add('delete');
-      deleteb.innerText = 'X';
-      article.appendChild(deleteb);
+        var li = document.createElement('li');
+        var article = document.createElement('article');
+        var button = document.createElement('button');
+        button.classList.add('check');
+        article.appendChild(button);
+        var p = document.createElement('p');
+        p.innerText = text;
+        article.appendChild(p);
+        var deleteb = document.createElement('button');
+        deleteb.classList.add('delete');
+        deleteb.innerText = 'X';
+        article.appendChild(deleteb);
 
-      li.appendChild(article);
-      document.querySelector('ul').appendChild(li);
+        li.appendChild(article);
+        document.querySelector('ul').appendChild(li);
 
     }
 
 
-  var myArray = [];
-  
-    document.querySelector('form').addEventListener('submit', function submit(event){
+    document.querySelector('form').addEventListener('submit', function submit(event) {
         event.preventDefault();
 
         addToDo(document.querySelector('.new-todo').value);
         console.log('hit it');
 
-        var todomo = document.querySelector('.new-todo').value;
+    });
 
-        var time = {
-          text: submit,
-          time: Date.now()
-        };
-        myArray.push(time);
-        console.log(myArray);
-        addToDo(todomo);
+    document.querySelector('ul').addEventListener('click', function complete(event) {
+        if (event.target.className === 'check') {
+            event.target.parentNode.classList.add('complete');
+        } else if (event.target.className === 'delete') {
+            event.target.parentNode.parentNode.parentNode.removeChild(event.target.parentNode.parentNode);
+        }
 
     });
+    window.todo.submit = addToDo;
 }());
