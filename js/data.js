2e4e61014c3be58ab36aee5f7744360129ace88f
J Ridgeway <sw333zy@gmail.com>
2016-11-23 19:41:06 -0500

    fixed iffes n moved code from data to ui

--- js/data.js
+++ js/data.js
@@ -1,47 +1,8 @@
-(function(){
+(function() {
+  'use strict';
   window.todo = window.todo || {};
 
-  function addToDo(text) {
-    var li = document.createElement('li');
-    var article = document.createElement('article');
-
-    var button = document.createElement('button');
-    button.classList.add('check');
-    article.appendChild(button);
-
-    var p = document.createElement('p');
-    p.innerText = text;
-    article.appendChild(p);
-
-    var deleteb = document.createElement('button');
-    deleteb.classList.add('delete');
-    deleteb.innerText = 'X';
-    article.appendChild(deleteb);
-
-    li.appendChild(article);
-    document.querySelector('ul').appendChild(li);
-
-  }
-
-  // addToDo('HEYYYYYYYY');
-
-
-
-  document.querySelector('form').addEventListener('submit', function submit(event){
-      event.preventDefault();
-
-      addToDo(document.querySelector('.new-todo').value)
-      console.log('hit it')
-  })
-
-
-
-
-
-  // my special code
-
-
-
+  
 
 
 }());
