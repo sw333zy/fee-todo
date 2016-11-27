07496c2116f0f987bd11237748e06f85262b6a91
J Ridgeway <sw333zy@gmail.com>
2016-11-27 14:37:01 -0500

    need splice implementation

--- js/ui.js
+++ js/ui.js
@@ -67,6 +67,40 @@
   function todoList() {
          document.querySelector('.incomplete-items').innerText = window.todo.listings();
        }
-  
+
 
 }());
+
+
+// implement and remove current counter
+//
+// function remove() {
+//     var id = this.getAttribute('id');
+//     var todos = get_todos();
+//     todos.splice(id, 1);
+//     localStorage.setItem('todo', JSON.stringify(todos));
+//
+//     show();
+//
+//     return false;
+// }
+//
+// function show() {
+//     var todos = get_todos();
+//
+//     var html = '<ul>';
+//     for(var i=0; i<todos.length; i++) {
+//         html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">x</button></li>';
+//     };
+//     html += '</ul>';
+//
+//     document.getElementById('todos').innerHTML = html;
+//
+//     var buttons = document.getElementsByClassName('remove');
+//     for (var i=0; i < buttons.length; i++) {
+//         buttons[i].addEventListener('click', remove);
+//     };
+// }
+//
+// document.getElementById('add').addEventListener('click', add);
+// show();
